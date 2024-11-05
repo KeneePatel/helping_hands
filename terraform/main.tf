a58e92ccfae79875b36baef263bae01f14c8ee65
KeneePatel <patelkenee2804@gmail.com>
2024-11-04 21:30:10 -0400

    pipeline fix: data source modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,36 @@
 # setting provider
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
   subscription_id = var.subscription_id
 }
 
-# making resource group
-resource "azurerm_resource_group" "frontend_backend_rg" {
-  name     = "frontend-backend-rg"
-  location = "East US"
+# Data source to reference the existing resource group
+data "azurerm_resource_group" "existing_rg" {
+  name = "frontend-backend-rg"
 }
 
-# Defining and setting a virtual network with subnet
-resource "azurerm_virtual_network" "frontend_backend_vnet" {
+# Data source to reference the existing virtual network
+data "azurerm_virtual_network" "existing_vnet" {
   name                = "frontend-backend-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.frontend_backend_rg.location
-  resource_group_name = azurerm_resource_group.frontend_backend_rg.name
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
 }
 
-resource "azurerm_subnet" "frontend_backend_subnet" {
+# Data source to reference the existing subnet
+data "azurerm_subnet" "existing_subnet" {
   name                 = "frontend-backend-subnet"
-  resource_group_name  = azurerm_resource_group.frontend_backend_rg.name
-  virtual_network_name = azurerm_virtual_network.frontend_backend_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
 }
 
 # Creating Network Interface (NIC)
 resource "azurerm_network_interface" "frontend_backend_nic" {
   name                = "frontend-backend-nic"
-  location            = azurerm_resource_group.frontend_backend_rg.location
-  resource_group_name = azurerm_resource_group.frontend_backend_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.frontend_backend_subnet.id
+    subnet_id                     = data.azurerm_subnet.existing_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.frontend_backend_pip.id
   }
@@ -46,8 +39,8 @@ resource "azurerm_network_interface" "frontend_backend_nic" {
 # Making the vm to be in a static ip
 resource "azurerm_public_ip" "frontend_backend_pip" {
   name                = "frontend-backend-pip"
-  location            = azurerm_resource_group.frontend_backend_rg.location
-  resource_group_name = azurerm_resource_group.frontend_backend_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -55,8 +48,8 @@ resource "azurerm_public_ip" "frontend_backend_pip" {
 # Creating Network Security Group (NSG)
 resource "azurerm_network_security_group" "frontend_backend_nsg" {
   name                = "frontend-backend-nsg"
-  location            = azurerm_resource_group.frontend_backend_rg.location
-  resource_group_name = azurerm_resource_group.frontend_backend_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
 
   security_rule {
     name                       = "AllowSSH"
@@ -104,8 +97,8 @@ resource "azurerm_network_interface_security_group_association" "frontend_backen
 # Creating the main Virtual Machine using azurerm_linux_virtual_machine
 resource "azurerm_linux_virtual_machine" "frontend_backend_vm" {
   name                  = "frontend-backend-vm"
-  location              = azurerm_resource_group.frontend_backend_rg.location
-  resource_group_name   = azurerm_resource_group.frontend_backend_rg.name
+  location              = data.azurerm_resource_group.existing_rg.location
+  resource_group_name   = data.azurerm_resource_group.existing_rg.name
   network_interface_ids = [azurerm_network_interface.frontend_backend_nic.id]
   size                  = "Standard_B1s"
   admin_username        = "azureuser"
@@ -157,8 +150,8 @@ resource "azurerm_linux_virtual_machine" "frontend_backend_vm" {
 # Setting up the Azure Container Registry (ACR)
 resource "azurerm_container_registry" "frontend_backend_acr" {
   name                = "frontendbackendacr"
-  resource_group_name = azurerm_resource_group.frontend_backend_rg.name
-  location            = azurerm_resource_group.frontend_backend_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   sku                 = "Basic"
   admin_enabled       = true
 
