046aefb1091113e49cd2ebf813157efb9b440fdf
KeneePatel <patelkenee2804@gmail.com>
2024-11-07 00:15:46 -0400

    updated size to B2s

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,7 +97,7 @@ resource "azurerm_linux_virtual_machine" "frontend_backend_vm" {
   location              = azurerm_resource_group.frontend_backend_rg.location
   resource_group_name   = azurerm_resource_group.frontend_backend_rg.name
   network_interface_ids = [azurerm_network_interface.frontend_backend_nic.id]
-  size                  = "Standard_B1s"
+  size                  = "Standard_B2s"
   admin_username        = "azureuser"
 
   admin_ssh_key {
